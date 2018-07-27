2d92c7d2ac0efe0ccaad96b25ab30cb41a8bfd1b
Randy Syring <randy@thesyrings.us>
2018-07-26 20:00:47 -0400

    test error route/view

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 import flask_webtest
+import pytest
+
 from app import app
 
 
@@ -14,3 +16,8 @@ class TestViews:
     def test_url_name(self):
         resp = self.client.get('/DerbyPy')
         assert resp.body == b'Hello DerbyPy!'
+
+    def test_error(self):
+        with pytest.raises(ValueError) as excinfo:
+            self.client.get('/error')
+        assert 'Deliberate error' in str(excinfo.value)
